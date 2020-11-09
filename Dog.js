edc925371396b63c3614388962c3d6efd122461d
kidcrusoe <65433385+kidcrusoe@users.noreply.github.com>
2020-11-09 14:28:14 +0700

    Add Dog module

---
+++ Dog.js
@@ -0,0 +1,12 @@
+let chalk = require('chalk');
+
+function Dog(name) {
+    this.name = name;
+    this.stomach = [];
+}
+
+Dog.prototype.sayHi = function() {
+    console.log('Hi, My name is ' + chalk.red(this.name));
+}
+
+module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,5 @@
+let Dog = require('./Dog')
+
+let dog = new Dog('Lu');
+
+dog.sayHi();
