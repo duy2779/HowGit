85124d8f87e5c2218b8833a42f12d81d672ba4bc
kidcrusoe <65433385+kidcrusoe@users.noreply.github.com>
2020-11-10 11:05:53 +0700

    Horse run method

--- Horse.js
+++ Horse.js
@@ -2,3 +2,7 @@ function Horse(name, speed){
     this.name = name;
     this.speed = speed;
 }
+
+Horse.prototype.run = function(){
+    console.log(this.name + ' running with speed ' + this.speed);
+}
